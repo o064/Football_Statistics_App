9aa7f395a9802bbc834eb72e34bf11b2e5213ab8
omar <om039919@gmail.com>
2025-02-11 23:59:50 +0200

    edit app

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cookieParser = require('cookie-parser');
-// const { checkUser,authAdmin,authUser} = require('./middleware/authorize');
-// const customErrorMiddleware = require('./middleware/customError');
-// const UserRoutes= require('./routes/userRoute');
-// const AdminRoutes= require('./routes/adminRoute');
-// const CartRoute= require('./routes/CartRoute');
+
 //config
 require('dotenv').config();
-// public style
-
+const PORT = process.env.PORT || 5000;
 // database connection
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
-app.use(express.static('public'));
-app.use(cookieParser());
+
 
 // routes
 
