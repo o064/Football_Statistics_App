db47159a7ca0c2d378c1d49406b982c921de2b33
omar <om039919@gmail.com>
2025-02-12 12:44:39 +0200

    comments added

--- routes/StatsRoute.js
+++ routes/StatsRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const route = express.Router();
 const {getStatsById,createStats} = require('../controllers/StatsController');
 
-route.get("/player/:id",getStatsById);
+route.get("/player/:id",getStatsById); // get players 
 route.post('/', createStats); // add  stats
 
 module.exports = route;
