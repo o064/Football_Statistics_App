92d17c1be9ce3c8485c201eab6aa4c027053a1e0
omar <om039919@gmail.com>
2025-03-01 20:57:36 +0200

    .

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PlayerRoute = require('./routes/PlayerRoute');
 const StatsRoute = require('./routes/StatsRoute');
 const TeamRoute = require('./routes/TeamRoute');
